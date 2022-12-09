cad06cac9c8c1e60746194fe20bef00a5584836e
Omojiba <tolu.omojiba@gmail.com>
2022-12-09 14:53:14 +0100

    created pdf viewer component

---
+++ src/components/pages/my-resume/PDFviewer.js
@@ -0,0 +1,75 @@
+import React from "react";
+import resume from "./resume.module.css";
+import { PictureAsPdf } from "@mui/icons-material";
+import { Document, Page, pdfjs } from "react-pdf";
+import { useState } from "react";
+import { ImArrowLeft, ImArrowRight } from "react-icons/im";
+import "./pdf.css";
+import { useEffect } from "react";
+
+//import { pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
+function PDFviewer() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function changePage(offSet) {
+    setPageNumber((prevPageNumber) => pageNumber + offSet);
+  }
+
+  function changePageBack() {
+    if (pageNumber > 1) {
+      changePage(-1);
+    } else {
+    }
+  }
+
+  function changePageNext() {
+    if (pageNumber < numPages) {
+      changePage(+1);
+    } else {
+    }
+  }
+  return (
+    <>
+      <div className={resume.pdfBox}>
+        <Document
+          file="https://res.cloudinary.com/jibz/image/upload/v1670576135/docs/Joshua_Chike_Resume_rf1ypx.pdf"
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page className={resume.page} pageNumber={pageNumber} />
+        </Document>
+        <div className={resume.pdfExtras}>
+          <p>
+            Page {pageNumber} of {numPages}
+          </p>
+          <div className={resume.buttons}>
+            <p
+              className={pageNumber > 1 ? resume.whiteColor : resume.greyColor}
+              onClick={changePageBack}
+            >
+              <ImArrowLeft />
+            </p>
+
+            <p
+              className={
+                pageNumber < numPages ? resume.whiteColor : resume.greyColor
+              }
+              onClick={changePageNext}
+            >
+              <ImArrowRight />
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PDFviewer;
--- src/components/pages/my-resume/Resume.jsx
+++ src/components/pages/my-resume/Resume.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import { StyledContainer } from "../../styled/containers/StyledContainer";
 import resume from "./resume.module.css";
 import { PictureAsPdf } from "@mui/icons-material";
-import { Document, Page, pdfjs } from "react-pdf";
-import { useState } from "react";
-import { ImArrowLeft, ImArrowRight } from "react-icons/im";
-import "./pdf.css";
+// import { Document, Page, pdfjs } from "react-pdf";
+// import { useState } from "react";
+// import { ImArrowLeft, ImArrowRight } from "react-icons/im";
+// import "./pdf.css";
 import { useEffect } from "react";
+import PDFviewer from "./PDFviewer";
 
 //import { pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+//pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 function Resume() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+  // const [numPages, setNumPages] = useState(null);
+  // const [pageNumber, setPageNumber] = useState(1);
   // const [file, setFile] = useState();
 
   // useEffect(() => {
@@ -28,28 +29,28 @@ function Resume() {
 
   // const [file, setFile]=("./resume.pdf");
   // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-    setPageNumber(1);
-  }
+  // function onDocumentLoadSuccess({ numPages }) {
+  //   setNumPages(numPages);
+  //   setPageNumber(1);
+  // }
 
-  function changePage(offSet) {
-    setPageNumber((prevPageNumber) => pageNumber + offSet);
-  }
+  // function changePage(offSet) {
+  //   setPageNumber((prevPageNumber) => pageNumber + offSet);
+  // }
 
-  function changePageBack() {
-    if (pageNumber > 1) {
-      changePage(-1);
-    } else {
-    }
-  }
+  // function changePageBack() {
+  //   if (pageNumber > 1) {
+  //     changePage(-1);
+  //   } else {
+  //   }
+  // }
 
-  function changePageNext() {
-    if (pageNumber < numPages) {
-      changePage(+1);
-    } else {
-    }
-  }
+  // function changePageNext() {
+  //   if (pageNumber < numPages) {
+  //     changePage(+1);
+  //   } else {
+  //   }
+  // }
   const HandlePdf = () => {
     try {
       fetch(
@@ -74,38 +75,7 @@ function Resume() {
     <main className={resume.main}>
       <StyledContainer>
         <div className={resume.container}>
-          <div className={resume.pdfBox}>
-            <Document
-              file="https://res.cloudinary.com/jibz/image/upload/v1670576135/docs/Joshua_Chike_Resume_rf1ypx.pdf"
-              onLoadSuccess={onDocumentLoadSuccess}
-            >
-              <Page className={resume.page} pageNumber={pageNumber} />
-            </Document>
-            <div className={resume.pdfExtras}>
-              <p>
-                Page {pageNumber} of {numPages}
-              </p>
-              <div className={resume.buttons}>
-                <p
-                  className={
-                    pageNumber > 1 ? resume.whiteColor : resume.greyColor
-                  }
-                  onClick={changePageBack}
-                >
-                  <ImArrowLeft />
-                </p>
-
-                <p
-                  className={
-                    pageNumber < numPages ? resume.whiteColor : resume.greyColor
-                  }
-                  onClick={changePageNext}
-                >
-                  <ImArrowRight />
-                </p>
-              </div>
-            </div>
-          </div>
+          <PDFviewer />
 
           <div id="download" className={resume.file} onClick={HandlePdf}>
             Download a copy <PictureAsPdf style={{ color: "red" }} />
